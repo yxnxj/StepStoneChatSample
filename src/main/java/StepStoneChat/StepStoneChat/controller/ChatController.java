90d74443dab30290c293bd034c61b4be95f8092c
YeonJun Cho <micayel1219@gmail.com>
2022-08-16 21:17:40 +0900

    code miss edit

--- src/main/java/StepStoneChat/StepStoneChat/controller/ChatController.java
+++ src/main/java/StepStoneChat/StepStoneChat/controller/ChatController.java
@@ -24,15 +24,15 @@ public class ChatController {
     /**
      * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
      */
-//    @MessageMapping("/chat/message")
-//    public void message(ChatMessage message) {
-//        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-//            chatRoomRepository.enterChatRoom(message.getRoomId());
-//            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-//        }
-//        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
-//        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
-//    }
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+    }
 
     /**
      * @MessageMapping 을 통해 WebSocket으로 들어오는 메시지 발행을 처리
